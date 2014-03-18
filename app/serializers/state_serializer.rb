db27b3accb98711961d0ef8e530daf5fb02ec7bc
Matthew Higgins <higginsmt@gmail.com>
2014-03-18 09:33:55 -0400

    mostly finished, awaiting help on map-switching

--- app/serializers/state_serializer.rb
+++ app/serializers/state_serializer.rb
@@ -1,17 +1,41 @@
 class StateSerializer < ActiveModel::Serializer
   attributes :name, :abbrev, :hs_rate, :hs_rank, :ba_rate, :ba_rank, :adv_rate, :adv_rank, :fill_color
 
-    def fill_color
-      case object.hs_rate
-      when 0..85 then
+
+  def fill_color
+
+    # case object.hs_rate
+    #   when 0..85 then
+    #     "LOW"
+    #   when 85..90 then
+    #     "MEDIUM"
+    #   when 90..100 then
+    #     "HIGH"
+    #   else
+    #     "UNDEFINED"
+    # end
+
+    # case object.adv_rate
+    #   when 0..85 then
+    #     "LOW"
+    #   when 85..90 then
+    #     "MEDIUM"
+    #   when 90..100 then
+    #     "HIGH"
+    #   else
+    #     "UNDEFINED"
+    # end
+
+    case object.ba_rate
+      when 0..24 then
         "LOW"
-      when 85..90 then
+      when 24..30 then
         "MEDIUM"
-      when 90..100 then
+      when 30..100 then
         "HIGH"
       else
         "UNDEFINED"
-      end
     end
 
+  end
 end
